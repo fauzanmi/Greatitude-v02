26ec80a570cf8d09e5fb590b3238ddf5625b72d5
Bagas099 <135189139+Bagas099@users.noreply.github.com>
2024-06-16 00:02:47 +0700

    Update Adapter & ViewModel
    create NoteAdapter, Note,ViewModel, NoteViewModelFactory and update MainActivity

--- app/src/main/java/com/tekber/greatitude/MainActivity.kt
+++ app/src/main/java/com/tekber/greatitude/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.tekber.greatitude.database.NoteDatabase
+import com.tekber.greatitude.repository.NoteRepository
+import com.tekber.greatitude.viewmodel.NoteViewModel
+import com.tekber.greatitude.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/tekber/greatitude/viewmodel/NoteViewModel.kt
@@ -0,0 +1,31 @@
+package com.tekber.greatitude.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.tekber.greatitude.model.Note
+import com.tekber.greatitude.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(app: Application, private val noteRepository: NoteRepository): AndroidViewModel(app) {
+
+    fun addNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.insertNote(note)
+        }
+
+    fun deleteNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
+
+    fun updateNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.updateNote(note)
+        }
+
+    fun getAllNotes() = noteRepository.getAllNotes()
+
+    fun searchNote(query: String?) =
+        noteRepository.searchNote(query)
+}
